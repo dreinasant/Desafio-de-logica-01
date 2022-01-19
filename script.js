071157ab7f0747a5f9e911cb492f7608987a9a3c
dreinasant <ao.andreina@gmail.com>
2022-01-19 19:55:23 -0300

    desafio de logica 1

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "Andreina"
+let numero = 21
+let anoAtual = 2022 - numero 
+console.log(anoAtual)
+
+console.log(numero + anoAtual)
+console.log( "Estou mexendo no console.log" )
+
+let quartaFeira = true
+console.log('O valor booleano é ${boolean}')
